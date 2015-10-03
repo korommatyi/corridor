b1037e861909b14d06cb3b05c16cda313f2e24ab
Matyas Korom <korommatyi@gmail.com>
2015-10-03 15:55:07 +0200

    Connect GUI with backend.

--- src/clj/corridor/handler.clj
+++ src/clj/corridor/handler.clj
@@ -1,10 +1,11 @@
 (ns corridor.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.response :refer [redirect]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (redirect "index.html"))
   (route/not-found "Not Found"))
 
 (def app
